5fbd646be11299a8a279e22c5f8d25dbc16025fb
dean <pnim.idf@gmail.com>
2022-09-28 12:47:06 +0300

    com2

--- hafazot_storage/hafazot_storage/settings.py
+++ hafazot_storage/hafazot_storage/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#b+0_f(8vo1(-3$za@qkd+%s1fts!zbm3t-l17t8cwce1t%pro'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
