c2c2247feef0d8c1d944566753c1b700a98681c6
dean <pnim.idf@gmail.com>
2022-10-03 12:29:17 +0300

    deployment

--- hafazot_storage/hafazot_storage/settings.py
+++ hafazot_storage/hafazot_storage/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,7 +127,10 @@ USE_TZ = True
 
 # STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media/'
 
